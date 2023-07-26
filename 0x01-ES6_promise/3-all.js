950984bff58c5f6b486aa740c1f45ed96d4df39f
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-07-26 07:57:21 +0100

    refactoring

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,19 @@
+/* eslint-disable camelcase */
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  const photo = uploadPhoto();
+  const user = createUser();
+  return Promise.all([photo, user])
     .then((results) => {
-      const [photoResult, userResult] = results;
-      console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
+      const new_res = results[0];
+      const { body } = new_res;
+      const res = results[1];
+      const first_name = res.firstName;
+      const last_name = res.lastName;
+      console.log(`${body} ${first_name} ${last_name}`);
     })
-    /* eslint-disable no-unused-vars */
     .catch(() => {
       console.log('Signup system offline');
     });
-  /* eslint-enable no-unused-vars */
 }
