9e91c3a8d07503f678aad93cddee0e62ac0c7fae
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-07-26 09:27:17 +0100

    refactoring

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,7 +6,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const photoPromise = uploadPhoto(fileName);
 
   return Promise.allSettled([userPromise, photoPromise]).then((results) => {
-    console.log(results);
     return results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason,
