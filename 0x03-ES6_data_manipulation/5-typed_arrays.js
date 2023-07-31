1991ceca9ab25a98d7cf56091766222c55c58090
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-07-31 23:49:12 +0100

    add task 5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,14 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const view = new Int8Array(buffer);
+
+  view[position] = value;
+
+  const dataView = new DataView(buffer);
+
+  return dataView;
+}
