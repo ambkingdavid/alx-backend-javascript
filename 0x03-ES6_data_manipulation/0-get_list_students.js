ca435f0bc7d4660e752bc5038ca4409ae32e68a1
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-07-31 20:32:00 +0100

    refactoring

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,25 @@
 const getListStudents = () => {
-  const student1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
-  const student2 = { id: 2, firstName: 'James', location: 'Columbia' };
-  const student3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
-  const newarr = [];
-  newarr.push(student1, student2, student3);
-  return newarr;
+  const student1 = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+
+  const student2 = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+
+  const student3 = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  const studentArray = [student1, student2, student3];
+
+  return studentArray;
 };
 
 export default getListStudents;
