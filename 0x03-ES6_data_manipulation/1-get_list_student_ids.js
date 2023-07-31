61c444a3521bc3a6707458f2ac40f183df43b899
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-07-31 21:59:25 +0100

    refactoring

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,15 +1,17 @@
 /* eslint-disable no-unused-vars */
+/* eslint-disable camelcase */
+
 import getListStudents from './0-get_list_students';
 
-const getListStudentsIds = (getListStudents) => {
+const getListStudentIds = (getListStudents) => {
   if (!Array.isArray(getListStudents)) {
     return [];
   }
-  const idArray = [];
-  for (const student of getListStudents) {
-    idArray.push(student.id);
-  }
-  return idArray;
+  const student_ids = [];
+  getListStudents.forEach((student_id) => {
+    student_ids.push(student_id.id);
+  });
+  return student_ids;
 };
 
-export default getListStudentsIds;
+export default getListStudentIds;
