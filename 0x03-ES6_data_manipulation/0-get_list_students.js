93d711e04ee94ab2247c766938306906bd5739d7
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-07-31 19:12:07 +0100

    eslint fix

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,11 @@
 export default function getListStudents() {
+  /* eslint-disable */
   const students = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
   ];
 
+  /* eslint-enable */
   return students;
 }
