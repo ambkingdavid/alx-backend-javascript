9829bd3d1eb43304fe1b35f3d0034bb7da6d77da
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-07-31 19:19:37 +0100

    reactored

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,11 @@
-export default function getListStudents(id, firstName, location) {
-  /* eslint-disable */
+/* eslint-disable */
+const getListStudents = (id, firstName, location) => {
   const student1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
   const student2 = { id: 2, firstName: 'James', location: 'Columbia' };
   const student3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
   const newarr = [];
   newarr.push(student1, student2, student3);
-  /* eslint-enable */
   return newarr;
-}
+};
+
+export default getListStudents;
