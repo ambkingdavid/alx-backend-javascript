bdf7e53c62b9812b8365b1febfa2d4ec9fdb611a
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-01 08:22:00 +0100

    refactoring

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
 export default function cleanSet(set, startString) {
-  let newString = ''
-  if (startString !== '') {
-    const regex = new RegExp(`^${startString}`, 'i');
-    const filteredValues = [...set].filter((value) => regex.test(value));
-    const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
-    newString = cleanedValues.join('-');
+  let newString = '';
+  if (startString === '' || startString === undefined) {
     return newString;
   }
+  const regex = new RegExp(`^${startString.trim()}`, 'i');
+  const filteredValues = [...set].filter((value) => regex.test(value));
+  const cleanedValues = filteredValues.map((value) => value.slice(startString.trim().length).trim());
+  newString = cleanedValues.join('-');
   return newString;
 }
