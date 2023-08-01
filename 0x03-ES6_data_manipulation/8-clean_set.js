f682ee47f197fa6fa77ba9d27f5533c0cb2d7170
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-01 07:22:24 +0100

    add task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,5 @@
+export default function cleanSet(set, startString) {
+  const filteredValues = [...set].filter((value) => value.startsWith(startString));
+  const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
+  return cleanedValues.join('-');
+}
