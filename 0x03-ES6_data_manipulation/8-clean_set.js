21d847b4c347f86bd0eded1090c08c4813c25df5
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-01 08:23:48 +0100

    eslint fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,7 +5,8 @@ export default function cleanSet(set, startString) {
   }
   const regex = new RegExp(`^${startString.trim()}`, 'i');
   const filteredValues = [...set].filter((value) => regex.test(value));
-  const cleanedValues = filteredValues.map((value) => value.slice(startString.trim().length).trim());
+  const cleanedValues = filteredValues.map((value) => value.slice(startString
+    .trim().length).trim());
   newString = cleanedValues.join('-');
   return newString;
 }
