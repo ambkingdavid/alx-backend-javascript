3665a6dd6c52991dfd5f2f72b16d52947362c971
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-01 07:44:20 +0100

    refactoring

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
-    return '';
+  if (startString !== '') {
+    const filteredValues = [...set].filter((value) => value.startsWith(startString));
+    const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
+    return cleanedValues.join('-');
   }
-  const filteredValues = [...set].filter((value) => value.startsWith(startString));
-  const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
-  return cleanedValues.join('-');
+
 }
