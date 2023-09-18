ac9f49b25e2503273a06d80aacd291fdb515bb28
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-09-18 21:35:47 +0100

    add changes

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
-console.log("Welcome to Holberton School, what is your name?");
+console.log(`Welcome to Holberton School` +
+`, what is your name?`);
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString().trim()}`);
 });
