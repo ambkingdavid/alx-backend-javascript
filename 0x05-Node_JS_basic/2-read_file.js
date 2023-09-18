5acf26a1f86c4e1182081b690cb325e516fa13aa
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-09-18 21:49:01 +0100

    add changes

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,40 +1,33 @@
+#!/usr/bin/node
+
+// Read a file synchronously with Node.js
 const fs = require('fs');
 
-function countStudents(path) {
+const countStudents = (CsvFilePath) => {
   try {
-    const data = fs.readFileSync(path, 'utf8');
-
-    const lines = data.split('\n');
-    const fields = {}
-    let totalStudents = 0;
-
-    for (let i = 1; i < lines.length; i++) {
-      const studentData = lines[i].split(',');
-      if (studentData && studentData.length === 4) {
-        const field = studentData[studentData.length - 1];
-        const firstName = studentData[0];
-        if (!fields[field]) {
-          fields[field] = {
-            count: 1,
-            list: [firstName]
-          };
-          totalStudents++;
-        } else {
-          fields[field].count++;
-          fields[field].list.push(firstName);
-          totalStudents++;
+    const csvData = fs.readFileSync(CsvFilePath, 'utf-8');
+    const rows = csvData.split('\n');
+    const headerRow = rows[0].split(',');
+    const fieldIndex = headerRow.indexOf('field');
+    const firstNameIndex = headerRow.indexOf('firstname');
+    if (fieldIndex !== -1) {
+      const csStudents = [];
+      const sweStudents = []; // To store the first names of SWE students
+      for (let i = 1; i < rows.length; i += 1) {
+        const row = rows[i].split(',');
+        if (row[fieldIndex] === 'SWE') {
+          sweStudents.push(row[firstNameIndex]);
+        } if (row[fieldIndex] === 'CS') {
+          csStudents.push(row[firstNameIndex]);
         }
       }
+      console.log(`Number of students: ${rows.length - 1}`);
+      console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+      console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
     }
-    console.log(`Number of students: ${totalStudents}`);
-    for (f in fields) {
-      console.log(`Number of students in ${f}: ${fields[f].count}.` +
-                    ` List: ${fields[f].list}`);
-    }
-
-  } catch (error) {
-    console.log('Cannot load the database');
+  } catch (err) {
+    throw new Error('Cannot load the database');
   }
-}
+};
 
 module.exports = countStudents;
