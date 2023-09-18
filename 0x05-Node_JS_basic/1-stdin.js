7a6e0f41649f5b94534ba49bbdbff2e21f6be443
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-09-18 20:47:29 +0100

    add changes

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,8 @@
-const handleUserInput = (input) => {
-  console.log(`Your name is: ${input}`);
-};
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', (input) => {
-  handleUserInput(input);
+const welcomeMessage = "Welcome to Holberton School, what is your name?";
+console.log(welcomeMessage);
+process.stdin.on('data', (data) => {
+  console.log(`Your name is: ${data}`);
 });
-process.on('exit', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  console.log("This important software is now closing");
 });
-module.exports = handleUserInput;
