eb8efaa5e7d6cdf58d90c168f2785a88db2c12bb
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-09-18 21:31:04 +0100

    add changes

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,29 +1,9 @@
-const readline = require('readline');
-
-// Create an interface for reading input from the user
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+#!/usr/bin/node
 
 console.log("Welcome to Holberton School, what is your name?");
-
-// Start a continuous loop to keep prompting for input
-function promptForName() {
-  rl.question('', (userInput) => {
-    if (userInput === undefined) {
-      // Ctrl+D was used to exit
-      console.log("This important software is now closing");
-      rl.close();
-    } else {
-      // Process the user's input
-      console.log(`Your name is: ${userInput}`);
-    }
-
-    // Continue the loop by calling the function recursively
-    promptForName();
-  });
-}
-
-// Start the continuous input loop
-promptForName();
+process.stdin.on('data', (data) => {
+  console.log(`Your name is: ${data.toString()}`);
+});
+process.stdin.on('end', () => {
+  console.log("This important software is now closing");
+});
