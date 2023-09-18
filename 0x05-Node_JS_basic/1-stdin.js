daea1490fe319d9c284a5791e301aa4fe7130034
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-09-18 20:56:02 +0100

    add changes

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 const welcomeMessage = "Welcome to Holberton School, what is your name?";
 console.log(welcomeMessage);
 process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data}`);
+  console.log(`Your name is: ${data.toString()}`);
 });
 process.stdin.on('end', () => {
   console.log("This important software is now closing");
