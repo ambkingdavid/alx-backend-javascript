6fb11558670b123b72fd019bdd24ae193a713e1e
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-09-19 08:56:05 +0100

    add changes

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,5 +3,5 @@ process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
 });
 process.stdin.on('close', () => {
-  process.stdout.write('This important software is now closing');
+  console.log('This important software is now closing');
 });
