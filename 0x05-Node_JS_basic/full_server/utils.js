2119eccb0f3ecec421d655de1e656c668a620bee
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-09-19 18:56:05 +0100

    add changes

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -12,7 +12,7 @@ function readDatabase(databasePath) {
     }
     fs.readFile(databasePath, 'utf8', (error, data) => {
       if (error) {
-        reject(new Error(('Cannot load the database')));
+        reject(error);
       } else {
         const lines = data.split('\n');
         const fields = {};
