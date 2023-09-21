15dbfc8a58dcb7e15b0fa27de569770383433179
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-09-21 19:07:32 +0100

    add tasks

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,5 +1,9 @@
-// 6-payment_token.js
-
+/**
+ * Get a payment token from the API.
+ *
+ * @param {boolean} success - Indicates whether the API request should succeed (true) or fail (false).
+ * @returns {Promise} A Promise that resolves with the API response or null.
+ */
 function getPaymentTokenFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
