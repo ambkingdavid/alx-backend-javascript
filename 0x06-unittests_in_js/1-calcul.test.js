258d7a7835dd9345f2e1bfa91229239003b18b41
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-09-21 09:18:17 +0100

    add tests

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -9,54 +9,50 @@ describe('calculateNumber', () => {
   });
   // Test cases for ADD type
   describe('ADD', () => {
-    it('should return 0 when adding two zeros', () => {
-      assert.strictEqual(calculateNumber('SUM', 0, 0), 0);
+    it('should return 4 for input values 1 and 3', () => {
+      assert.strictEqual(calculateNumber(1, 3), 4);
     });
-
-    it('should add positive numbers correctly', () => {
-      assert.strictEqual(calculateNumber('SUM', 5, 10), 15);
+  
+    it('should return -4 for input values -1 and -3', () => {
+      assert.strictEqual(calculateNumber(-1, -3), -4);
     });
-
-    it('should add negative numbers correctly', () => {
-      assert.strictEqual(calculateNumber('SUM', -5, -10), -15);
+  
+    it('should return 5 for input values 1.2 and 3.4', () => {
+      assert.strictEqual(calculateNumber(1.2, 3.4), 4);
     });
-
-    it('should handle decimal numbers by rounding', () => {
-      assert.strictEqual(calculateNumber('SUM', 2.3, 4.7), 7);
+  
+    it('should return 5 for input values 1.5 and 3.7', () => {
+      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
-
-    it('should add a large positive number to a small negative number', () => {
-      assert.strictEqual(calculateNumber('SUM', 1000000, -0.000001), 1000000);
+  
+    it('should return 5 for input values 1.49 and 3.51', () => {
+      assert.strictEqual(calculateNumber(1.49, 3.51), 5);
     });
-
-    it('should add a large negative number to a small positive number', () => {
-      assert.strictEqual(calculateNumber('SUM', -1000000, 0.000001), -1000000);
+  
+    it('should return 3e20 for input values 1e20 and 2e20', () => {
+      assert.strictEqual(calculateNumber(1e20, 2e20), 3e20);
     });
-
-    it('should handle adding numbers with different precisions', () => {
-      assert.strictEqual(calculateNumber('SUM', 0.1, 0.2), 0);
+  
+    it('should return 0 for input values 0 and 0', () => {
+      assert.strictEqual(calculateNumber(0, 0), 0);
     });
-
-    it('should handle adding zero to a large positive number', () => {
-      assert.strictEqual(calculateNumber('SUM', 1000000, 0), 1000000);
+  
+    it('should return 4 for input values 1.25 and 2.25', () => {
+      assert.strictEqual(calculateNumber(1.25, 2.25), 3);
     });
-
-    it('should handle adding zero to a large negative number', () => {
-      assert.strictEqual(calculateNumber('SUM', -1000000, 0), -1000000);
-    });
-
-    it('should add numbers where one is very close to zero', () => {
-      assert.strictEqual(calculateNumber('SUM', 0.000001, 0.000002), 0);
+  
+    it('should return 4 for input values 1.25 and 2.75', () => {
+      assert.strictEqual(calculateNumber(1.25, 2.75), 4);
     });
   });
 
   // Test cases for SUBTRACT type
   describe('SUBTRACT', () => {
-    it('should return 0 when subtracting zero from zero', () => {
+    it('should return 0 when (0 - 0)', () => {
       assert.strictEqual(calculateNumber('SUBTRACT', 0, 0), 0);
     });
 
-    it('should subtract positive numbers correctly', () => {
+    it('should return 5 when (15 - 10)', () => {
       assert.strictEqual(calculateNumber('SUBTRACT', 15, 10), 5);
     });
 
@@ -68,28 +64,24 @@ describe('calculateNumber', () => {
       assert.strictEqual(calculateNumber('SUBTRACT', 4.7, 2.3), 3);
     });
 
-    it('should subtract a large positive number from a small negative number', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', 0.000001, -1000000), 1000000);
+    it('should handle decimal numbers by rounding', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', -4.7, 2.3), -7);
     });
 
     it('should subtract a large negative number from a small positive number', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', 0.000001, 1000000), -1000000);
+      assert.strictEqual(calculateNumber('SUBTRACT', 1e20, 2e20), -1e20);
     });
 
-    it('should handle subtracting numbers with different precisions', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', 0.2, 0.1), 0); // Rounded to 1 decimal place
+    it('should handle subtracting decimal numbers', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', -4.7, -2.3), -3);
     });
 
     it('should handle subtracting a large positive number from zero', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', 0, 1000000), -1000000);
+      assert.strictEqual(calculateNumber('SUBTRACT', -1e20, 2e20), -3e20);
     });
 
     it('should handle subtracting zero from a large negative number', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', -1000000, 0), -1000000);
-    });
-
-    it('should subtract numbers where one is very close to zero', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', 0.000002, 0.000001), 0);
+      assert.strictEqual(calculateNumber('SUBTRACT', -1e20, -2e20), 1e20);
     });
   });
 
