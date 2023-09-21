c195f904c00de604d95bec339108a0b084b2e424
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-09-21 09:20:17 +0100

    add tests

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -10,39 +10,39 @@ describe('calculateNumber', () => {
   // Test cases for ADD type
   describe('ADD', () => {
     it('should return 4 for input values 1 and 3', () => {
-      assert.strictEqual(calculateNumber(1, 3), 4);
+      assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
     });
   
     it('should return -4 for input values -1 and -3', () => {
-      assert.strictEqual(calculateNumber(-1, -3), -4);
+      assert.strictEqual(calculateNumber('SUM', -1, -3), -4);
     });
   
     it('should return 5 for input values 1.2 and 3.4', () => {
-      assert.strictEqual(calculateNumber(1.2, 3.4), 4);
+      assert.strictEqual(calculateNumber('SUM', 1.2, 3.4), 4);
     });
   
     it('should return 5 for input values 1.5 and 3.7', () => {
-      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+      assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
     });
   
     it('should return 5 for input values 1.49 and 3.51', () => {
-      assert.strictEqual(calculateNumber(1.49, 3.51), 5);
+      assert.strictEqual(calculateNumber('SUM', 1.49, 3.51), 5);
     });
   
     it('should return 3e20 for input values 1e20 and 2e20', () => {
-      assert.strictEqual(calculateNumber(1e20, 2e20), 3e20);
+      assert.strictEqual(calculateNumber('SUM', 1e20, 2e20), 3e20);
     });
   
     it('should return 0 for input values 0 and 0', () => {
-      assert.strictEqual(calculateNumber(0, 0), 0);
+      assert.strictEqual(calculateNumber('SUM', 0, 0), 0);
     });
   
     it('should return 4 for input values 1.25 and 2.25', () => {
-      assert.strictEqual(calculateNumber(1.25, 2.25), 3);
+      assert.strictEqual(calculateNumber('SUM', 1.25, 2.25), 3);
     });
   
     it('should return 4 for input values 1.25 and 2.75', () => {
-      assert.strictEqual(calculateNumber(1.25, 2.75), 4);
+      assert.strictEqual(calculateNumber('SUM', 1.25, 2.75), 4);
     });
   });
 
